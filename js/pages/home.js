332b5242c1ca22c5e0487af9397f08e5ca30e17f
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-03-12 22:17:53 +0200

    Basics

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,1 +1,21 @@
+// IMPORTS
 
+// EXECUTION
+
+/* HEADER: start */
+/* HEADER: end */
+
+/* HERO: start */
+/* HERO: end */
+
+/* CHOOSE US: start */
+/* CHOOSE US: end */
+
+/* METRICS: start */
+/* METRICS: end */
+
+/* LATEST VIDEO: start */
+/* LATEST VIDEO: end */
+
+/* FOOTER: start */
+/* FOOTER: end */
